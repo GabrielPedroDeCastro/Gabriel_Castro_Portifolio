8f70e1de8303a0e603b99c3e34cee6c00a4495ce
Gabriel Pedro de Castro <96214858+GabrielPedroDeCastro@users.noreply.github.com>
2023-05-06 17:35:59 -0300

    2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,3 +164,6 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+
+
